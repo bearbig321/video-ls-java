0b22498397b55d9586755ff95f486fa275186f62
bearbig321 <128585591+bearbig321@users.noreply.github.com>
2026-02-09 16:59:15 +0800

    Fix service impl mapper imports

--- src/main/java/com/example/videols/service/impl/LearningProgressServiceImpl.java
+++ src/main/java/com/example/videols/service/impl/LearningProgressServiceImpl.java
@@ -1,11 +1,11 @@
 package com.example.videols.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.example.videols.entity.${s};
-import com.example.videols.mapper.${s}Mapper;
-import com.example.videols.service.${s}Service;
+import com.example.videols.entity.LearningProgress;
+import com.example.videols.mapper.LearningProgressMapper;
+import com.example.videols.service.LearningProgressService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ${s}ServiceImpl extends ServiceImpl<${s}Mapper, ${s}> implements ${s}Service {
+public class LearningProgressServiceImpl extends ServiceImpl<LearningProgressMapper, LearningProgress> implements LearningProgressService {
 }
